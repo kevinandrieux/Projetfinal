ddc98696bfc66ee1a4ae0ac4f8f7987e36b56e63
kevinandrieux <50326816+kevinandrieux@users.noreply.github.com>
2021-03-04 09:56:52 +0100

    accueil fait style footer  categorie

---
+++ accueil.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="./CSS/carrousel.css">
+      <main>
+        <section class="description">
+          <h1 style="padding: 20px;">Description du site</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime aliquam laboriosam reprehenderit ullam iusto dignissimos incidunt, vitae optio impedit autem tempore, molestiae omnis temporibus soluta id consequuntur cum odit nobis?
+          Obcaecati sapiente, aliquid error pariatur in sint inventore at quae molestias earum exercitationem officia repellendus aspernatur assumenda sequi illum fugit, eaque, libero voluptatibus iure commodi perspiciatis repudiandae. Eos, reprehenderit ab.
+          Impedit mollitia placeat cumque? Optio error blanditiis necessitatibus cumque hic, rerum possimus impedit quaerat sed dolores ut nam est eaque omnis quis minima perspiciatis eos facere porro. Asperiores, doloremque doloribus.
+          Soluta deleniti nobis laboriosam voluptatem, dolor tenetur atque repellat eveniet expedita inventore minus dolorum. Corrupti id repellat, numquam, perspiciatis nulla doloribus libero dolorum, officiis quos magnam harum tempore rem accusantium.
+          Quod sapiente, deserunt debitis magnam minima nobis rem explicabo aliquam quis accusantium dolor, dicta amet id ducimus omnis cupiditate eos, veritatis temporibus labore natus excepturi. Facere iure assumenda alias deserunt.</p>
+        </section>
+        <section class="slider">
+          <base href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/">
+<div id="slider">
+<figure>
+<img src="austin-fireworks.jpg" alt>
+<img src="taj-mahal_copy.jpg" alt>
+<img src="ibiza.jpg" alt>
+<img src="ankor-wat.jpg" alt>
+<img src="austin-fireworks.jpg" alt>
+</figure>
+</div>
+<aside>
+  <div style="padding-bottom: 12px;">
+  <input type="text" value="recherche" class="recherche">
+</div>
+<div class="recent" style="padding-bottom: 12px;">
+  <h1>Liste article récent</h1>
+</div>
+  <div class="article">
+    <div>
+    <img class='imagerecent' src="austin-fireworks.jpg" alt="">
+  </div>
+    <div class="prix"><h3>nom de l'article</h3>
+    <p>prix</p></div> 
+  </div>
+
+</aside>
+        </section>
+      </main>
