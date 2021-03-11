a11bbad57a97e1abce703bf26b6ae35a1f5edd97
Verin <aradon@wanadoo.fr>
2021-03-11 15:15:47 +0100

    css

--- Sport.php
+++ Sport.php
@@ -3,7 +3,7 @@ include ("./Include/header.php");
 ?>
 
 <link rel="stylesheet" href="./CSS/card.css">
-
+<main class="list_art">
 <section class="section">
 <a href="./sports_collectifs.php">
 <div class="card">
@@ -51,24 +51,7 @@ include ("./Include/header.php");
 </div>
 </div>
 </a>
-<a href="./sport_raquette.php">
-<div class="card">
-    <img src="./img/sport_raquette.jfif" alt=""  class="img">
-    <div class="card2">
-    <h4>Sports de raquettes</h4>
-    <p>Vous trouverez dans cette rubrique les annonces concernant les articles:</p>
-</div>
-</div>
-</a>
-<a href="./sport_raquette.php">
-<div class="card">
-    <img src="./img/sport_raquette.jfif" alt=""  class="img">
-    <div class="card2">
-    <h4>Sports de raquettes</h4>
-    <p>Vous trouverez dans cette rubrique les annonces concernant les articles:</p>
-</div>
-</div>
-</a>
+
 <a href="./sport_raquette.php">
 <div class="card">
     <img src="./img/sport_raquette.jfif" alt=""  class="img">
@@ -88,8 +71,23 @@ include ("./Include/header.php");
 </div>
 </a>
 </section>
+<aside>
+  <div style="padding-bottom: 12px;">
+  <input type="text" value="recherche" class="recherche">
+</div>
+<div class="recent" style="padding-bottom: 12px;">
+  <h1>Liste article récent</h1>
+</div>
+  <div class="article">
+    <div>
+    <img class='imagerecent' src="./img/BannièreJV.jpg" alt="">
+  </div>
+    <div class="prix"><h3>nom de l'article</h3>
+    <p>prix</p></div> 
+  </div>
 
-
+</aside>
+</main>
 
 
 
