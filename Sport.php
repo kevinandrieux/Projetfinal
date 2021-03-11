46b00470768ffbc009818d01e2ffa560f08a818b
CaronAurelien <aurelcaron01@gmail.com>
2021-03-11 11:01:47 +0100

    ajout_card

--- Sport.php
+++ Sport.php
@@ -2,6 +2,38 @@
 include ("./Include/header.php");
 ?>
 
+<link rel="stylesheet" href="./CSS/card.css">
+
+
+<a href="./sports_collectifs.php">
+<div class="card">
+<img src="./img/sport_collectif.jpg" alt=""  class="img">
+    <h4>Sports collectifs</h4>
+    <p>Vous trouverez dans cette rubrique les annonces concernant les articles:</p>
+</div>
+</a>
+
+
+
+<a href="./sports_individuels.php">
+<div class="card">
+<img src="./img/sport_individuel.jfif" alt=""  class="img">
+    <h4>Sports individuel</h4>
+    <p>Vous trouverez dans cette rubrique les annonces concernant les articles:</p>
+</div>
+</a>
+
+
+
+
+
+<a href="./sport_raquette.php">
+<div class="card">
+<img src="./img/sport_raquette.jfif" alt=""  class="img">
+    <h4>Sports de raquettes</h4>
+    <p>Vous trouverez dans cette rubrique les annonces concernant les articles:</p>
+</div>
+</a>
 
 
 
