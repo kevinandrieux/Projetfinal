fb93df5361cc5f3b4f7e7d4af58362894c2c11b2
Verin <aradon@wanadoo.fr>
2021-03-12 11:43:57 +0100

    template JV+

--- indexjv.php
+++ indexjv.php
@@ -3,37 +3,37 @@
 ?>
 <div class="container_indexinfo">
 
-<h1 class="title_info">Matériel Informatique</h1>
+<h1 class="title_info">Jeux-Vidéos</h1>
 
 <div class="card-container_indexinfo">
     <div class="hover_indexinfo">
-            <a href="./indexinfo_port.php">
+            <a href="./indexjv_console.php">
                 <div class="card_indexinfo">
                     <img src="./img/Info/portable.png" alt="Random photo" class="card-img_indexinfo">
                     <div class="card-body_indexinfo">
-                        <p>Ordinateurs Portables</p>
+                        <p>Consoles</p>
                     </div>
                 </div>
             </a>
         </div>
 
     <div class="hover_indexinfo">
-            <a href="./indexinfo_fix.php">
+            <a href="./indexjv_jeux.php">
                 <div class="card_indexinfo">
                     <img src="./img/Info/fixe.png" alt="Random photo" class="card-img_indexinfo">
                     <div class="card-body_indexinfo">
-                        <p>Ordinateurs Fixes</p>
+                        <p>Jeux-Vidéos</p>
                     </div>
                 </div>
             </a>
         </div>
 
     <div class="hover_indexinfo">
-            <a href="./indexinfo_periph.php">
+            <a href="./indexjv_access.php">
                 <div class="card_indexinfo">
                     <img src="./img/Info/périphériques.png" alt="Random photo" class="card-img_indexinfo">
                     <div class="card-body_indexinfo">
-                        <p>Périphériques & Accessoires</p>
+                        <p>Accessoires</p>
                     </div>
                 </div>
             </a>
