fd604bdeba54c59422ae30a7fa93863f21373360
Verin <aradon@wanadoo.fr>
2021-03-12 11:18:35 +0100

    template_jv

--- indexjv.php
+++ indexjv.php
@@ -1,13 +1,49 @@
 <?php
-    include './Include/header.php';
+    include ('./Include/header.php');
 ?>
+<div class="container_indexinfo">
 
+<h1 class="title_info">Matériel Informatique</h1>
 
+<div class="card-container_indexinfo">
+    <div class="hover_indexinfo">
+            <a href="./indexinfo_port.php">
+                <div class="card_indexinfo">
+                    <img src="./img/Info/portable.png" alt="Random photo" class="card-img_indexinfo">
+                    <div class="card-body_indexinfo">
+                        <p>Ordinateurs Portables</p>
+                    </div>
+                </div>
+            </a>
+        </div>
 
+    <div class="hover_indexinfo">
+            <a href="./indexinfo_fix.php">
+                <div class="card_indexinfo">
+                    <img src="./img/Info/fixe.png" alt="Random photo" class="card-img_indexinfo">
+                    <div class="card-body_indexinfo">
+                        <p>Ordinateurs Fixes</p>
+                    </div>
+                </div>
+            </a>
+        </div>
 
+    <div class="hover_indexinfo">
+            <a href="./indexinfo_periph.php">
+                <div class="card_indexinfo">
+                    <img src="./img/Info/périphériques.png" alt="Random photo" class="card-img_indexinfo">
+                    <div class="card-body_indexinfo">
+                        <p>Périphériques & Accessoires</p>
+                    </div>
+                </div>
+            </a>
+    </div>
+</div>
+</div>
 
 
 
 
 <?php
-    
+    include ('./Include/footer.php');
+?>
