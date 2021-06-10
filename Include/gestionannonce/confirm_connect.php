962d43b52b02f27c5b873022a03acaf0bbf0be49
kevinandrieux <50326816+kevinandrieux@users.noreply.github.com>
2021-06-10 11:04:01 +0200

    tableau

--- Include/gestionannonce/confirm_connect.php
+++ Include/gestionannonce/confirm_connect.php
@@ -10,6 +10,62 @@ if(isset($_SESSION['username'])){
 ?>
 </section>
 
+<table>
+<tr>
+<th>
+nom
+</th>
+<th>
+prix
+</th>
+<th>
+date
+</th>
+<th>
+categorie
+</th>
+<th>
+Modifier/Supprimer
+</th>
+</tr>
+<?php
+require '../database.php';
+    $username = $_SESSION['username'];
+$req = $pdo->query("SELECT * FROM `annonces` WHERE role_user='$username'
+");
+
+    while($data = $req->fetch()){
+        echo'
+        <tr>
+        <td>
+'.$data->nom.'
+</td>
+<td>
+'.$data->prix.'
+</td>
+<td>
+'.$data->datatime.'
+</td>
+<td>
+'.$data->role_cat.'
+</td>
+<td>
+<a style="color: white;" href="./modifier.php?id='.$data->id.'">Modifier</a>
+<a style="color: white;" href="./supprimer.php?id='.$data->id.'">Supprimer</a>
+</td>
+</tr>';
+
+    }
+
+?>
+
+
+</table>
+
+
+
+</div>
+
 <nav class="memb_nav">
 <section class="memb_sect">
 <h3>Poster une annonce:</h3>
